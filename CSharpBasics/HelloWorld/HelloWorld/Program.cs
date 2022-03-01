63e5769d3571fc5b4218cebc67fefa7c82b8654e
stomaras <sptoma724@gmail.com>
2022-03-01 12:12:41 +0200

    CSharp Basics

---
+++ CSharpBasics/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        // Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Tom!");
+        }
+    }
+}
