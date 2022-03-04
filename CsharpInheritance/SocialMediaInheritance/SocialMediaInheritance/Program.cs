4218d4f75a7f5931ba890679b3e2db969d315efb
stomaras <sptoma724@gmail.com>
2022-03-04 15:29:18 +0200

    Inheritance Post example creation of Post class with getters and setters , with 4 properties and 2 methods one override method ToString from System.Object and another method with name Update(string title, bool isPublic)

---
+++ CsharpInheritance/SocialMediaInheritance/SocialMediaInheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SocialMediaInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post();
+            Console.WriteLine(post.ToString());
+            Post post2 = new Post("Left-Wing", true, "Chris Fragulis");
+            Console.WriteLine(post2.ToString());
+        }
+    }
+}
